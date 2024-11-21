f0398cf77bb91f86cac1c7aadcbae86403191e74
onyii-e <onyinyechinwaneri00@gmail.com>
2024-11-21 02:10:04 +0000

    6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements (page1, page2) {
+export default function mergeDeeplyElements(page1, page2) {
   const mapOne = new Map(page1);
   const mapTwo = new Map(page2);
 
